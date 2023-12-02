b693cfe42efa12404fc850bb1fda2bbb3e4e4d53
Alex <alek.gasowski@gmail.com>
2023-12-02 14:50:48 +0000

    committing new text boxes

--- first_Prototype/scripts/script.js
+++ first_Prototype/scripts/script.js
@@ -42,16 +42,22 @@ var input;
 
 // here is where the prompt is acquired
 function submitPrompt(){
-    input = document.getElementById("prompt").value
-    const answ = askGpt(input);
+    input = document.getElementById("prompt").value;
+    displayUserPrompt(input); // new way to display what the user has written
+
+    displayAIAnswer("This is AI answering") // EXAMPLE of displaying AI answer, remove it when not needed
+    // const answ = askGpt(input);
 }
 
 // here a link will be used to upload a text file of the book
 function submitLink(){
-    link = document.getElementById("link").value
+    link = document.getElementById("link").value;
 }
 
-// example functionality
+
+
+// !!!!!!!!!!!!!!!!!
+// OBSOLETE FUNCTION, USE displayUserPrompt() and displayAIAnswer()
 function addChatElement(outputString){
     
     // creates p element and adds it to chat div with prompt value
@@ -63,36 +69,15 @@ function addChatElement(outputString){
     
 }
 
+
+
 function submitSettings(){
 
-    lockSettings();
-
+    lockSettings();     // locking settings input boxes for the duration of upload
     uploadSettings();   // this method MUST be between lockSettings() and showChat()
-
-    showChat();
+    showChat();         // after settings are uploaded the UI will switch from settings panel to chat panel
 }
 
-// show chat and hide settings
-function showChat(){
-    document.getElementById("settings-window").style.display = "none"
-    document.getElementById("chat-window").style.display = "block"
-}
-
-// show settings and hide chat
-function showSettings(){
-    document.getElementById("chat-window").style.display = "none"
-    document.getElementById("settings-window").style.display = "block"
-}
-
-// this function is a safety mechanism that disables all input boxes during the wait time between clicking the button and getting an answer from chatgpt
-function lockSettings(){
-    // all setting boxes will be turned off so no input can be changed
-    let list = document.getElementsByClassName("setting");
-    for (let i = 0; i < list.length; i++){
-        list[i].disabled = true
-    }
-    
-}
 
 // here the settings get sent to chatgpt
 function uploadSettings(){
@@ -103,5 +88,57 @@ function uploadSettings(){
 
     
     // compile the settings and send it to Chat-GPT 
+    // place here all functionality related to the first prompt which applies settings of the ChatGPT
 }
 
+// calling this function displays what the user prompts in the chat 
+function displayUserPrompt(text){
+    let box = document.createElement("div");
+    box.classList.add("chat-prompt-display");
+    spawnChatBox(text, box)
+}
+
+// calling this function display what the AI answeres in the chat
+function displayAIAnswer(text){
+    let box = document.createElement("div");
+    box.classList.add("chat-answer-display");
+    spawnChatBox(text, box)
+}
+
+
+
+// this just constructs html structure
+// its for UI only, ignore it
+function spawnChatBox(text, box){
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text;
+
+    box.appendChild(paragraph)
+    let chatDiv = document.getElementById("chat");
+
+    chatDiv.appendChild(box);
+}
+
+// this function is a safety mechanism that disables all input boxes during the wait time between clicking the button and getting an answer from chatgpt
+// its for UI only, ignore it
+function lockSettings(){
+    // all setting boxes will be turned off so no input can be changed
+    let list = document.getElementsByClassName("setting");
+    for (let i = 0; i < list.length; i++){
+        list[i].disabled = true
+    }
+}
+
+// show chat and hide settings
+// its for UI only, ignore it
+function showChat(){
+    document.getElementById("settings-window").style.display = "none"
+    document.getElementById("chat-window").style.display = "block"
+}
+
+// show settings and hide chat
+// its for UI only, ignore it
+function showSettings(){
+    document.getElementById("chat-window").style.display = "none"
+    document.getElementById("settings-window").style.display = "block"
+}
