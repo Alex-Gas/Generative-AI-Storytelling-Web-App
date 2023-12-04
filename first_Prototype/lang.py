b9452b69ddcf92c1ba9f59a41fa8b92faa10baf5
marwanosman505 <marwanosman505@gmail.com>
2023-12-04 18:25:54 +0000

    successfully persisted chatbot memory

--- first_Prototype/lang.py
+++ first_Prototype/lang.py
@@ -11,26 +11,30 @@ from langchain.prompts import (
 )
 
 # LLM
-llm = ChatOpenAI(open_api_key="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")
+llm = ChatOpenAI(api_key="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")
 
-# # Prompt
-# prompt = ChatPromptTemplate(
-#     messages=[
-#         SystemMessagePromptTemplate.from_template(
-#             "You are a nice chatbot having a conversation with a human."
-#         ),
-#         # The `variable_name` here is what must align with memory
-#         MessagesPlaceholder(variable_name="chat_history"),
-#         HumanMessagePromptTemplate.from_template("{question}"),
-#     ]
-# )
+# Prompt
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a nice chatbot having a conversation with a human."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ]
+)
 
-# # Notice that we `return_messages=True` to fit into the MessagesPlaceholder
-# # Notice that `"chat_history"` aligns with the MessagesPlaceholder name
-# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-# conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
+# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
 
-# # Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
-# conversation({"question": "hi"})
+# Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
+print("Ask a question:")
+for i in range (5):
+    question = input()
+    answer = conversation({"question": question})
+    print(answer)
 
-# print(memory)
+print(memory)
