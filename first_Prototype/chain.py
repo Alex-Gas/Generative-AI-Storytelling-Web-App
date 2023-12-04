d6eb3a641b95f3c9257a5f53bc9242ffb16b9696
marwanosman505 <marwanosman505@gmail.com>
2023-12-04 17:55:21 +0000

    multiple chains

---
+++ first_Prototype/chain.py
@@ -0,0 +1,62 @@
+from operator import itemgetter
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+
+prompt1 = ChatPromptTemplate.from_template("what is the city {person} is from?")
+prompt2 = ChatPromptTemplate.from_template(
+    "what country is the city {city} in? respond in {language}"
+)
+
+model = ChatOpenAI(api_key="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")
+
+chain1 = prompt1 | model | StrOutputParser()
+
+chain2 = (
+    {"city": chain1, "language": itemgetter("language")}
+    | prompt2
+    | model
+    | StrOutputParser()
+)
+
+chain2.invoke({"person": "obama", "language": "spanish"})
+
+from langchain.schema.runnable import RunnablePassthrough
+
+prompt1 = ChatPromptTemplate.from_template(
+    "generate a {attribute} color. Return the name of the color and nothing else:"
+)
+prompt2 = ChatPromptTemplate.from_template(
+    "what is a fruit of color: {color}. Return the name of the fruit and nothing else:"
+)
+prompt3 = ChatPromptTemplate.from_template(
+    "what is a country with a flag that has the color: {color}. Return the name of the country and nothing else:"
+)
+prompt4 = ChatPromptTemplate.from_template(
+    "What is the color of {fruit} and the flag of {country}?"
+)
+
+model_parser = model | StrOutputParser()
+
+color_generator = (
+    {"attribute": RunnablePassthrough()} | prompt1 | {"color": model_parser}
+)
+color_to_fruit = prompt2 | model_parser
+color_to_country = prompt3 | model_parser
+question_generator = (
+    color_generator | {"fruit": color_to_fruit, "country": color_to_country} | prompt4
+)
+
+print(question_generator.invoke("warm"))
+
+print(chain2.invoke({"person": "obama", "language": "spanish"}))
+
+class promptChain:
+    def __init__(self) -> None:
+        pass
+    def addPrompt()
+
+#recurring prompt
+newPrompt = ChatPromptTemplate("Previous Response({}) ")
+
